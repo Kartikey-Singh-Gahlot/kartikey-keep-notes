a0a15b67537f34579e39509b221b9fed70e48ae1
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-12-02 12:43:44 +0530

    debugging issue with signout issue

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -76,7 +76,7 @@ const signOut = async (req, res)=>{
         maxAge : 7*24*60*60*1000,
     }
     
-     res.cookie('authCookie',"Cleared", cookieDetails);
+     res.cookie('ClearedCookie',"Cleared", cookieDetails);
      res.status(200).json({
       httpOnly: true,
       status:true,
