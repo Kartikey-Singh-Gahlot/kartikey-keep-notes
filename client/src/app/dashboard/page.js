62857954e0cb2fcdba05fbf0b6c8ffd08744714f
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-12-02 09:51:07 +0530

    checking for dashboard redirection isssue

--- client/src/app/dashboard/page.js
+++ client/src/app/dashboard/page.js
@@ -18,7 +18,6 @@ export default function Dashboard(){
             setUserDetails(pr.body);
             return;
         }
-        router.push("/auth/signin")
       }
       getCredentials();
     },[])
