b6015556b2ccde5ad1b3e85a9c234890e73916dc
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-12-02 12:12:14 +0530

    solved auth cookie problem 02

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -69,7 +69,13 @@ const signin = async (req, res)=>{
 
 const signOut = async (req, res)=>{
     try{
-     const cookieDetails = { httpOnly:true , secure: true, sameSite: "None", path: "/" }
+     const cookieDetails = {
+        httpOnly: true,
+        secure: true,     
+        sameSite: "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: "/"
+     }
      res.clearCookie('authCookie', cookieDetails);
      res.status(200).json({
       httpOnly: true,
