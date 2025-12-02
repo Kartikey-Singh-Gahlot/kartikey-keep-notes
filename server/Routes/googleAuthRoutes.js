1705a2a91cb9f3760340cabb1e3cc9f14e806eaf
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-12-02 11:30:46 +0530

    rectifying authcookie issue

--- server/Routes/googleAuthRoutes.js
+++ server/Routes/googleAuthRoutes.js
@@ -15,7 +15,7 @@ googleRouter.get("/auth/google/callback", passport.authenticate("google", {sessi
       const token = jwt.sign({id:req.user._id, email:req.user.email}, process.env.SECRETKEY, {expiresIn:"7d"});
       const cookieDetails = {
         httpOnly: true,
-        secure: false,     
+        secure: true,     
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000
       }
