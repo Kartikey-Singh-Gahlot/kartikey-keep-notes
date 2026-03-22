81425687237ec62885a091af31fadf1440831083
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2026-03-22 20:34:21 +0530

    styled subject dashboard

--- client/src/app/components/sbjectsPresenter.js
+++ client/src/app/components/sbjectsPresenter.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 export function ExploreSubjectsPresenter(){
-    let [subjects, setSubjects] = useState([{name:"", description:"", chapters:[], createdAt:""}]);
+    let [subjects, setSubjects] = useState([{name:"", description:"", chapters:[], createdAt:"", imageUrl:"/mainBgBlackImageOne.png", likesCount:0}]);
     async function getAllSubjects(){
       const un = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/subjects`,{method:"GET", credentials:"include", headers: { "Content-Type": "application/json"}});
       const pr = await un.json();
@@ -25,10 +25,11 @@ export function ExploreSubjectsPresenter(){
               subjects.map((subject, index)=>{
                   return(          
                       <Link href={'/auth/signin'} key={index} className={`min-w-[280px] max-w-[400px] flex flex-col w-full transition-colors text-nowrap px-4 py-2 rounded-[4px]  text-white hover:border-green-800 hover:bg-amber-50 hover:text-green-800 border border-[#ffffff00]  bg-green-800  cursor-pointer`}>
-                                    <div className="w-full flex items-center justify-between">
+                                 <div className="w-full flex items-center justify-between">
                                         <h2 className="text-xl font-bold underline">{subject.name}</h2>
                                         <h2 className="text-sm text-amber-50">Likes :{subject.likesCount}</h2>
                                   </div>
+                                  <img src={subject.imageUrl} className="w-full h-40 object-cover my-2 rounded"/>
                                   <p className="w-full text-wrap">
                                      {subject.description}
                                   </p>
@@ -41,7 +42,7 @@ export function ExploreSubjectsPresenter(){
 }
 
 export function NormalSubjectsPresenter(){
-    let [subjects, setSubjects] = useState([{name:"", description:"", chapters:[], createdAt:""}]);     
+    let [subjects, setSubjects] = useState([{name:"", description:"", chapters:[], createdAt:"", imageUrl:"/mainBgBlackImageOne.png"}]);     
     async function getAllSubjects(){
       const un = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/subjects`,{method:"GET", credentials:"include", headers: { "Content-Type": "application/json"}});
       const pr = await un.json();   
@@ -67,6 +68,7 @@ export function NormalSubjectsPresenter(){
                                         <h2 className="text-xl font-bold underline">{subject.name}</h2>
                                         <h2 className="text-sm text-amber-50">Likes :{subject.likesCount}</h2>
                                     </div>  
+                                  <img src={subject.imageUrl} className="w-full h-40 object-cover my-2 rounded"/>
                                     <p className="w-full text-wrap">
                                      {subject.description}
                                     </p>
--- server/Models/subjectModel.js
+++ server/Models/subjectModel.js
@@ -4,6 +4,7 @@ const subjectSchema = new mongoose.Schema({
   name:{type:String, default:'General', unique:true, required:true},
   description:{type:String, default: function get(){return `This is basic ${this.name} subject`}},
   chapters:{type:[mongoose.Schema.Types.ObjectId], ref:'Chapters', default:[]},
+  imageUrl:{type:String, default:"./mainBgBlackImageOne.png"},
   likesCount:{type:Number, default:0},
   createdAt:{type:Date, default:Date.now}
 })
