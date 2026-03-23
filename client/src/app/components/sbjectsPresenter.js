301295364d6c38d2d14833e6fa92e89735a95c3c
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2026-03-23 22:36:05 +0530

    added styling for landing page notes presenter

--- client/src/app/components/sbjectsPresenter.js
+++ client/src/app/components/sbjectsPresenter.js
@@ -20,16 +20,16 @@ export function ExploreSubjectsPresenter(){
     },[]);
 
     return(
-          <ul className="flex gap-5 w-full flex-wrap justify-start py-5">
+          <ul className="flex gap-5 w-full justify-start py-5 overflow-x-scroll scroll-smooth scroll-hidden max-w-[98vw] ">
             {
               subjects.map((subject, index)=>{
                   return(          
-                      <Link href={'/auth/signin'} key={index} className={`min-w-[280px] max-w-[400px] flex flex-col w-full transition-colors text-nowrap px-4 py-2 rounded-[4px]  text-white hover:border-green-800 hover:bg-amber-50 hover:text-green-800 border border-[#ffffff00]  bg-green-800  cursor-pointer`}>
-                                 <div className="w-full flex items-center justify-between">
+                      <Link href={'/auth/signin'} key={index} className={`min-w-[350px] max-w-[400px]  flex flex-col w-full transition-colors text-nowrap px-4 py-2 rounded-[4px]  text-white hover:border-green-800 hover:bg-amber-50 hover:text-green-800 border border-[#ffffff00]  bg-green-800  cursor-pointer`}>
+                                 <div className="w-full flex items-center justify-between pb-5">
                                         <h2 className="text-xl font-bold underline">{subject.name}</h2>
                                         <h2 className="text-sm text-amber-50">Likes :{subject.likesCount}</h2>
                                   </div>
-                                  <img src={subject.imageUrl} className="w-full h-40 object-cover my-2 rounded"/>
+                                  <img src={subject.imageUrl} className="w-full  object-cover my-2 rounded"/>
                                   <p className="w-full text-wrap">
                                      {subject.description}
                                   </p>
