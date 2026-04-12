5c160ac94f865bb24184b32a38b515e4b17cb83c
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2026-04-12 21:31:46 +0530

    organized styling

--- client/src/components/loader.js
+++ client/src/components/loader.js
@@ -1,30 +1,18 @@
-import "./components.css";
 
-
-export  function FullScreenLoader({theme}){
-    
-    
+export  function FullScreenLoader({theme}){    
     return(
-      <section className={`h-screen flex justify-center items-center  gap-4`}>
-            
-              <div className={`animate-spin h-10 w-10 rounded-full border-[6px] border-dotted  ${(theme)?"border-green-800  border-b-transparent border-r-transparent":"border-amber-50 border-b-transparent border-r-transparent"}`}>
-              </div>
+      <section className={`h-screen flex justify-center items-center  gap-4`}> 
+              <div className={`animate-spin h-10 w-10 rounded-full border-[6px] border-dotted  ${(theme)?"border-green-800  border-b-transparent border-r-transparent":"border-amber-50 border-b-transparent border-r-transparent"}`} />
               <h1 className="text-2xl ">Loading</h1>
-            
       </section>  
     )
 }
 
-export  function SectionalLoader({theme}){
-    
-    
+export  function SectionalLoader({theme}){    
     return(
       <section className={`h-full flex justify-center items-center  gap-4`}>
-            
-              <div className={`animate-spin h-10 w-10 rounded-full border-[6px] border-dotted  ${(theme)?"border-green-800  border-b-transparent border-r-transparent":"border-amber-50 border-b-transparent border-r-transparent"}`}>
-              </div>
+              <div className={`animate-spin h-10 w-10 rounded-full border-[6px] border-dotted  ${(theme)?"border-green-800  border-b-transparent border-r-transparent":"border-amber-50 border-b-transparent border-r-transparent"}`}/>
               <h1 className="text-2xl ">Loading</h1>
-            
       </section>  
     )
 }
--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import "./components.css";
 
 export default function Logo(){
     return(
