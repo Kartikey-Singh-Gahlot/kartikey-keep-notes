1aca58a71aca84c8887593d6dc06d6b442da27e3
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2026-07-08 21:20:09 +0530

    added basic gateway spring setup

---
+++ spring-server/api-gateway/src/main/java/api-gateway/ApiGateway.java
@@ -0,0 +1,13 @@
+package "api-gateway";
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
